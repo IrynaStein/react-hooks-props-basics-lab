aceb5dbe0c81fe4ab2a853e2a4d6eb3354d7d830
Iryna Stein <isophiestein@gmail.com>
2021-06-15 13:22:18 -0600

    Completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+    console.log(props)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* conditional statement */}
+      {props.bio && <p>{props.bio}</p>}
+      {/* {props.renderBio()} */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.github} linkedin={props.linkedin}/>
       {/* add your <Links /> component here */}
     </div>
   );
