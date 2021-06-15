b9b9ab895a47a08c79c55dfae4f8fb4a511bcd5e
Iryna Stein <isophiestein@gmail.com>
2021-06-15 13:22:06 -0600

    commit tree 638ee3198b7572695b5e86e3bf17a61347152a76

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,24 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
+console.log(user.links.github)
 
 function App() {
+  // function renderBio(){
+  //   if (user.bio){
+  //     return <p>{user.bio}</p>
+  //   }
+  // }
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.name} name={user.name} city={user.city} color={user.color}/> 
+      {/* <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} renderBio={renderBio}/> */}
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
